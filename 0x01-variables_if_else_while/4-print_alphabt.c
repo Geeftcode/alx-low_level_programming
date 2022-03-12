a69ecccc2273f047cd5fc554b5f0b456e4c1f24a
Geeftcode <giftokeoghene001@gmail.com>
2022-03-11 16:01:43 -0800

    print alphabeth

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * block of code
+ * print all alphabeth except q and e 
+ * Return: 0
+ */
+int main(void)
+{
+	int letter;
+
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter == 'q' || letter == 'e')
+		{
+			letter++;
+			continue;
+		}
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
