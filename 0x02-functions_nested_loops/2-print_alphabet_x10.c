86ceaf4fa5e6279bd677ca51060fee81e5e8c2e0
Geeftcode <giftokeoghene001@gmail.com>
2022-03-15 15:09:41 -0700

    lowercase x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main -entry point
+ * a program to print out lowercase alphabet
+ * in ten places
+ */
+void print_alphabet_x10(void)
+{
+	int count, alphabet;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+			_putchar(alphabet);
+		_putchar('\n');
+	}
+}
