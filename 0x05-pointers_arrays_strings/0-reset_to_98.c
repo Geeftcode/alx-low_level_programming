1beaa29e6b8b7b9ede32131b9f63d2891a4dcdef
Geeftcode <giftokeoghene001@gmail.com>
2022-03-21 06:23:30 -0700

    swap

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,10 +4,10 @@
 /**
  * main -  a function that takes a pointer to an int
  * as parameter and updates the value it points to to 98.
- * Return: 0 always
+ * @n: pointer to the variable to update
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
 }
-
