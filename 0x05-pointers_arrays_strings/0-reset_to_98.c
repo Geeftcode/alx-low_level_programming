1101e6b794a7968c65502b65ebacf28ab431209d
Geeftcode <giftokeoghene001@gmail.com>
2022-03-21 05:14:23 -0700

    pointers and reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,20 +7,14 @@
  * Return: 0 always
  */
 
-void reset_to_98(int *n) \\ resets the value of n to 98
-{
-printf("value of n:%p\n", &n);
-}
 int main(void)
 {
 	int n;
-	int *p;
 
 	n = 402;
-	p = &n;
-	printf("the value of n is %d\n", n);
+	_putchar("n=%d\n", n);
 	reset_to_98(&n);
 	n = 98;
-	printf("value of n is %d\n", n);
+	_putchar("n=%d\n", n);
 	return (0);
 }
