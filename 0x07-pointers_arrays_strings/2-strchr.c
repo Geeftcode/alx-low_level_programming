d0135b67cf37960008806fd48f92c311226570af
Geeftcode <giftokeoghene001@gmail.com>
2022-03-28 07:47:00 -0700

    first commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+include "main.h"
+/**
+ * *_strchr: locates a character in string
+ * @s: string
+ * @c: character
+ * Return: Returns a pointer to the first occurrence of the character c 
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+	
+	for (; *(s + i) != '\0'; i++)
+		if *(s + i) == c;
+	return (s + i);
