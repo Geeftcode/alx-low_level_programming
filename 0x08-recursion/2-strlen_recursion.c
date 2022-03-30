493b9f028c27c5d168dd85db2f6a9f669d6c9265
Geeftcode <giftokeoghene001@gmail.com>
2022-03-30 10:22:09 -0700

    lenhgt

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 /**
  * _strlen_recursion -  a function that returns the length of a string.
  * @s: string
- * Return : return 0
+ * Return: the lenght of a string
  */
 int _strlen_recursion(char *s)
 {
