8ca4d06b85726239814690bf78acbd33e6580ec9
Geeftcode <giftokeoghene001@gmail.com>
2022-04-14 06:48:51 -0700

    variadic prototyes

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -18,10 +18,6 @@ int sum_them_all(const unsigned int n, ...)
 
 	unsigned int i; sum = 0;
 	
-	if (n == 0)
-	{
-		putchar(0);
-	}
 	for(i = 0; i < n; i++)
 	       sum += va_arg(ap, unsigned int);
 va_end(ap);
