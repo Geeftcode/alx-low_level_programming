2d8bf1f055cec5636228a5805d1fb58d40afb214
geeftcode <giftokeoghene001@gmail.com>
2022-05-03 00:39:07 +0100

    updated task 1 and 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -41,23 +41,23 @@ int main(int argc, char *argv[])
         exit(97);
     }
     file_from = open(argv[1], O_RDONLY);
-    file_to = open(argv[2], O_CREAT |O_WRONLY | O_TRUNC | O_APPEND, 0664);
+    file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
     error_file(file_from, file_to, argv);
 
     nchars = 1024;
     while (nchars == 1024)
     {
-        nchars = read(file_from, buf,1024);
+        nchars = read(file_from, buf, 1024);
         if (nchars == -1)
         error_file(-1, 0, argv);
         nwr = write(file_to, buf, nchars);
         if (nwr == -1)
         error_file(0, -1, argv);
     }
-    err_close = close(file_from)
+    err_close = close(file_from);
     if (err_close == -1)
     {
-        dprintf(STDERR_FILENO, "Error: Can't closse fd %d\n", file_from);
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
         exit(100);
     }
     err_close = close(file_to);
